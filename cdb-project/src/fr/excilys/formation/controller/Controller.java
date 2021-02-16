a5b6e6313b9d02479a37a64759b464db5399de31
Alex Lagneau <alagneau@excilys.com>
2021-02-16 18:00:47 +0100

    Création de la Vue et du Controller avec pour l'instant l'affichage des listes de computer et companies et un début pour l'affichage en détail d'un computer; il reste à faire les pages permettant à l'utilisateur de sélectionner l'ordinateur

--- cdb-project/src/fr/excilys/formation/controller/Controller.java
+++ cdb-project/src/fr/excilys/formation/controller/Controller.java
@@ -1,16 +1,38 @@
 package fr.excilys.formation.controller;
 
-import fr.excilys.formation.model.Model;
+import java.util.ArrayList;
+import java.util.List;
+
+import fr.excilys.formation.model.Company;
+import fr.excilys.formation.model.Computer;
+import fr.excilys.formation.service.DAOCompany;
+import fr.excilys.formation.service.DAOComputer;
 
 public class Controller {
-	Model model;
+	private DAOCompany serviceCompany;
+	private DAOComputer serviceComputer;
 	
 	public Controller() {
-		model = Model.getInstance();
+		serviceCompany = new DAOCompany();
+		serviceComputer = new DAOComputer();
+	}
+
+	public List<Computer> getComputers(int offset, int rows) {
+		List<Computer> computers = new ArrayList<Computer>();
+		computers = serviceComputer.getAllComputers(offset, rows);
+		
+		return computers;
 	}
 	
-	public void update() {
-		model = Model.getInstance();
+	public List<Company> getCompanies(int offset, int rows) {
+		List<Company> companies = new ArrayList<Company>();
+		companies = serviceCompany.getAllCompanies(offset, rows);
+		
+		return companies;
+	}
+	
+	public Computer getComputerByID(int id) {
+		return serviceComputer.getComputerByID(id);
 	}
 	
 	
--- cdb-project/src/fr/excilys/formation/view/View.java
+++ cdb-project/src/fr/excilys/formation/view/View.java
@@ -1,34 +1,169 @@
 package fr.excilys.formation.view;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import fr.excilys.formation.controller.Controller;
+import fr.excilys.formation.model.Company;
+import fr.excilys.formation.model.Computer;
 
 public class View {
 	private Controller controller;
+	private int offset = 0, numberOfRows = 10;
+
+	private enum Page {
+		HOME("0"), COMPUTERS("1"), COMPANIES("2"), DETAIL("3"), CREATE("4"), UPDATE("5"), DELETE("6");
+
+		public final String label;
+		public static final Map<String, Page> association = new HashMap<>();
+
+		static {
+			for (Page page : values()) {
+				association.put(page.label, page);
+			}
+		}
+
+		private Page(String label) {
+			this.label = label;
+		}
+	};
+
+	private Page actualPage = Page.HOME;
 
 	public View() {
 		controller = new Controller();
 		System.out.println("Bienvenue sur MyComputerDatabase.com !\n\n");
+		displayPage();
 	}
-	
-	private void printMenu() {
-		System.out.println("Que souhaitez-vous faire ?");
-	}
-	
+
 	public int update(String query) {
-		switch(query) {
-		case "Q":
-		case "q":
+		int res = 1;
+		switch (actualPage) {
+		default:
+		case HOME:
+			res = pageHome(query);
+			break;
+		case COMPUTERS:
+			pageListDatas(query);
+			break;
+		case COMPANIES:
+			pageListDatas(query);
+			break;
+		case DETAIL:
+			pageDetail(query);
+			break;
+		case CREATE:
+			break;
+		case UPDATE:
+			break;
+		case DELETE:
+
+		}
+		displayPage();
+		return res;
+	}
+
+	private void displayPage() {
+		switch (actualPage) {
+		case HOME:
+			printMenu();
+			break;
+		case COMPUTERS:
+			printAllComputers();
+			break;
+		case COMPANIES:
+			printAllCompanies();
+			break;
+		case DETAIL:
+			break;
+		case CREATE:
+			break;
+		case UPDATE:
+			break;
+		case DELETE:
+		}
+	}
+
+	private int pageHome(String query) {
+		if (query.equals("q") || query.equals("Q")) {
 			System.out.println("\n\nAu plaisir de vous revoir !");
 			return 0;
-		case "1":
-			System.out.println("feature pas encore développée");
+		} else if (Page.association.containsKey(query)) {
+			actualPage = Page.association.get(query);
+		} else {
+			System.out.println("Nous ne connaissons pas cette commande :/");
+			printMenu();
+		}
+
+		return 1;
+	}
+
+	private void printMenu() {
+		System.out.println("Que souhaitez-vous faire ?");
+		System.out.println("0 : Accueil");
+		System.out.println("1 : Afficher tous les ordinateurs");
+		System.out.println("2 : Afficher toutes les entreprises");
+		System.out.println("3 : Afficher les informations sur un ordinateur");
+		System.out.println("4 : Créer un nouvel ordinateur");
+		System.out.println("5 : Modifier un ordinateur");
+		System.out.println("6 : Supprimer un ordinateur");
+
+	}
+
+	private void pageListDatas(String query) {
+		switch (query) {
+		case "P":
+			if (offset > 0) {
+				offset = (offset > 9) ? offset - 10 : 0;
+				System.out.println("Infos précedentes");
+			} else {
+				System.out.println("Nous ne pouvons pas revenir en arrière");
+			}
+			break;
+		case "N":
+			offset += 10;
+			System.out.println("Infos suivantes");
+			break;
+		case "A":
+			actualPage = Page.HOME;
+			offset = 0;
+			System.out.println("Switch to HOME");
 			break;
 		default:
-			System.out.println("Nous ne connaissons pas cette commande :/");
+			System.out.println("Commande non reconnue");
 		}
+	}
+
+	private void printAllComputers() {
+		List<Computer> computers = null;
+		String format = "%4d %-40s%s%n";
+
+		computers = controller.getComputers(offset, numberOfRows);
+
+		System.out.printf("%-4s %-40s%s%n", "ID", "Nom", "Entreprise");
+
+		for (Computer computer : computers) {
+			System.out.printf(format, computer.getID(), computer.getName(), computer.getCompany().getName());
+		}
+
+		System.out.println("N : Page suivante | P : Page Précédente | A : Accueil");
+	}
+
+	private void printAllCompanies() {
+		List<Company> companies = null;
+
+		companies = controller.getCompanies(offset, numberOfRows);
+
+		System.out.println("Entreprise");
+
+		for (Company company : companies) {
+			System.out.println(company.getName());
+		}
+
+		System.out.println("N : Page suivante | P : Page Précédente | A : Accueil");
+	}
+
+	private void pageDetail(String query) {
 		
-		printMenu();
-		
-		return 1;
 	}
 }
